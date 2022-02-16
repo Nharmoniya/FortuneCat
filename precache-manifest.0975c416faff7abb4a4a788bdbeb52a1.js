48d511871cd2657752d580493d000933caf16a0f
Nature <874572491@qq.com>
2022-02-16 13:00:48 +0800

    deploy

---
+++ precache-manifest.0975c416faff7abb4a4a788bdbeb52a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c88bb261b065b0afae6",
+    "url": "/FortuneCat/css/app.3ec056ec.css"
+  },
+  {
+    "revision": "addaa41b4dcbdecf8210536da6875b21",
+    "url": "/FortuneCat/fortunecat.png"
+  },
+  {
+    "revision": "69de531a6fc32b6697b10dd4b53031f9",
+    "url": "/FortuneCat/index.html"
+  },
+  {
+    "revision": "4c88bb261b065b0afae6",
+    "url": "/FortuneCat/js/app.ab65927d.js"
+  },
+  {
+    "revision": "1440c11bdb802b886e79",
+    "url": "/FortuneCat/js/chunk-vendors.0feb00f3.js"
+  },
+  {
+    "revision": "c0c04a46284578b124a6db8811e92e84",
+    "url": "/FortuneCat/manifest.json"
+  },
+  {
+    "revision": "f6af4ff477ab88869cea607b31245971",
+    "url": "/FortuneCat/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/FortuneCat/robots.txt"
+  }
+]);
